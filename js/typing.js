15a623055aae996738919b13ca18e753fe0417cf
pk1260 <43113381+pk1260@users.noreply.github.com>
2021-06-22 22:13:46 +0200

    Added --Color vars for easier edditing if styles change -- Scroll animation at hero banner -- typing js file

---
+++ js/typing.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    // Waits 1000ms after typing "First"
+    strings: ['a Software developer.', 'a Designer.', 'Professional.',  'Driven.'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
