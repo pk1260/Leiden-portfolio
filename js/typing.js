612c59f74b401d97d36a8ae011acfb15f14ef497
pk1260 <43113381+pk1260@users.noreply.github.com>
2021-06-23 21:32:43 +0200

    Added -- Added new image's -- edited css, html, js

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 var typed = new Typed('.type', {
     // Waits 1000ms after typing "First"
-    strings: ['a Software developer.', 'a Designer.', 'Professional.',  'Driven.'],
+    strings: ['a Software developer.', 'a Designer.', 'a Professional.',  'Driven.'],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true,
